276fb246fe9654d07153b29dcf8df067f633b656
Sam Shiffman <sam@samshiffman.com>
2015-03-10 11:43:45 -0700

    create third class

--- lcd_number_gen/lcd.rb
+++ lcd_number_gen/lcd.rb
@@ -38,107 +38,119 @@ else
   number = ARGV[0]
 end
 
-class LcdSizer
-	
-  attr_reader	:height
-	
-  def initialize(number, height)
-    @height = height.to_i
-    @digits = number.chars
+class LcdPrinter
+
+  attr_reader :lcd_row_printer, :height
+
+  def initialize(number, lcd_height)
+    @lcd_row_printer = LcdRowPrinter.new(number, lcd_height)
   end
+
+  def print_all_rows # print all lcd number rows
+    lcd_row_printer.print_row1
+    lcd_row_printer.height.times { lcd_row_printer.print_row2 }
+    lcd_row_printer.print_row3
+    lcd_row_printer.height.times { lcd_row_printer.print_row4 }
+    lcd_row_printer.print_row5
+  end
+
+end 
+
+class LcdRowPrinter
 	
+  attr_reader :height, :lcd_sizer
+
+  def initialize(number, height)
+    @digits = number.chars
+    @height = height.to_i
+    @lcd_sizer = LcdSizer.new(height)
+  end
+
   def print_row1 # print row 1 of 5 lcd number rows
-    print_row do |n|		
-      if n == "1" || n == "4" then print_blank
-      else print_horizontal_line
+    print_row do |n|    
+      if n == "1" || n == "4" then lcd_sizer.print_blank
+      else lcd_sizer.print_horizontal_line
       end
     end
   end
-			
+      
   def print_row2 # print row 2 of 5 lcd number rows
-    print_row do |n|		
+    print_row do |n|    
       case n
-      when "0", "4", "8".."9" then print_left_and_right
-      when "5".."6" then print_left_vertical
-      else print_right_vertical # when "1" , "2..3", "7"
+      when "0", "4", "8".."9" then lcd_sizer.print_left_and_right
+      when "5".."6" then lcd_sizer.print_left_vertical
+      else lcd_sizer.print_right_vertical # when "1" , "2..3", "7"
       end
     end
   end
 
   def print_row3 # print row 3 of 5 lcd number rows
-    print_row do |n|		
-      if n == "0" || n == "1" || n == "7" then print_blank
-      else print_horizontal_line # when "2".."6" , "8".."9"
+    print_row do |n|    
+      if n == "0" || n == "1" || n == "7" then lcd_sizer.print_blank
+      else lcd_sizer.print_horizontal_line # when "2".."6" , "8".."9"
       end
     end
   end
 
   def print_row4 # print row 4 of 5 lcd number rows
-    print_row do |n|		
+    print_row do |n|    
       case n
-      when "0", "6", "8" then print_left_and_right
-      when "2" then print_left_vertical							
-      else print_right_vertical # when "1", "3".."5", "7", "9"
+      when "0", "6", "8" then lcd_sizer.print_left_and_right
+      when "2" then lcd_sizer.print_left_vertical             
+      else lcd_sizer.print_right_vertical # when "1", "3".."5", "7", "9"
       end
     end
   end
 
   def print_row5 # print row 5 of 5 lcd number rows
-    print_row do |n|		
-      if n == "1" || n == "4" || n == "7" then print_blank
-      else print_horizontal_line # when "0", "2".."3" , "5".."6", "8".."9"
+    print_row do |n|    
+      if n == "1" || n == "4" || n == "7" then lcd_sizer.print_blank
+      else lcd_sizer.print_horizontal_line # when "0", "2".."3" , "5".."6", "8".."9"
       end
-    end	
+    end 
   end
 
   private
-	
-  def print_blank
-    print " #{' '* @height} "
-  end
-	
-  def print_horizontal_line
-    print " #{"-"* @height} "
-  end
-	
-  def print_left_vertical # print left-side upright
-    print "|#{' ' * @height} "
-  end
-		
-  def print_right_vertical # print right-side upright
-    print " #{' ' * @height}|"
-  end
-	
-  def print_left_and_right # print both uprights
-    print "|#{' ' * @height}|"
-  end
-	
-  def print_row(&block) # eliminates repetition in print_rowx
-    @digits.each do |n|
-      block.call(n)
-      print " "
+
+    def print_row(&block) # eliminates repetition in print_rowx
+      @digits.each do |n|
+        block.call(n)
+        print " "
+      end
+      puts
     end
-    puts
-  end
+
   
 end
 
-class LcdPrinter
+class LcdSizer
 
-  attr_reader	:lcd_sizer , :height
+  attr_reader :height
 
-  def initialize(number, lcd_height)
-    @lcd_sizer = LcdSizer.new(number, lcd_height)
+  def initialize(height)
+    @height = height.to_i
   end
 
-  def print_all_rows # print all lcd number rows
-    lcd_sizer.print_row1
-    lcd_sizer.height.times { lcd_sizer.print_row2 }
-    lcd_sizer.print_row3
-    lcd_sizer.height.times { lcd_sizer.print_row4 }
-    lcd_sizer.print_row5
+  def print_blank
+    print " #{' '* @height} "
   end
-
+  
+  def print_horizontal_line
+    print " #{"-"* @height} "
+  end
+  
+  def print_left_vertical # print left-side upright
+    print "|#{' ' * @height} "
+  end
+    
+  def print_right_vertical # print right-side upright
+    print " #{' ' * @height}|"
+  end
+  
+  def print_left_and_right # print both uprights
+    print "|#{' ' * @height}|"
+  end
+  
 end
 
 p = LcdPrinter.new(number, lcd_height)
